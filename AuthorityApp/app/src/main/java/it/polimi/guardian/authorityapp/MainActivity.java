7470ed7b3794638e0810f12d08a98549a4ee45c9
nemtajo <nemtajo@yahoo.com>
2015-04-07 13:34:03 +0200

    dodati aktiviti za button click

--- AuthorityApp/app/src/main/java/it/polimi/guardian/authorityapp/MainActivity.java
+++ AuthorityApp/app/src/main/java/it/polimi/guardian/authorityapp/MainActivity.java
@@ -2,38 +2,47 @@ package it.polimi.guardian.authorityapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+    private Button btn_takeAjob;
+    private Button btn_reviewAjob;
+    private Button btn_highscores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_takeAjob = (Button) findViewById(R.id.btn_take_a_job);
+        btn_takeAjob.setOnClickListener(this);
+        btn_reviewAjob = (Button) findViewById(R.id.btn_review_a_job);
+        btn_reviewAjob.setOnClickListener(this);
+        btn_highscores = (Button) findViewById(R.id.btn_highscores);
+        btn_highscores.setOnClickListener(this);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.btn_take_a_job:
+                Intent i1 = new Intent(this,TakeAJobActivity.class);
+                startActivity(i1);
+                break;
+            case R.id.btn_review_a_job:
+                Intent i2 = new Intent(this,ReviewAJobActivity.class);
+                startActivity(i2);
+                break;
+            case R.id.btn_highscores:
+                Intent i3 = new Intent(this,HighscoresActivity.class);
+                startActivity(i3);
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
